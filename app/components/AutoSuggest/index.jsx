9069545e0a8129955d099cb9855434a9f00fe0a0
Steven Straatemans <steven.straatemans@company.info>
2017-03-19 19:40:20 +0100

    Autosuggest

--- app/components/AutoSuggest/index.jsx
+++ app/components/AutoSuggest/index.jsx
@@ -1,69 +1,20 @@
 import React from 'react';
-import Rx, {Subject} from 'rxjs/Rx';
-import axios from 'axios';
-
 
 class AutoSuggest extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {search: []};
-
-
-    this.keyup$ = new Subject();
-    this.keyup$.map((e) => {
-      return e.target.value;
-    })
-    .filter((val) => {
-      return val.length > 2;
-    })
-    .debounceTime(750)
-    .distinctUntilChanged()
-    .subscribe((val) => {
-      return axios.get(`http://localhost:3000/shows?q=${val}`).then((d) => {
-        console.log(d.data);
-        this.setState({search: d.data});
-      });
-
-    });
-      // .map(function (e) {
-      //   return e.target.value; // Project the text from the input
-      // })
-      // .filter(function (text) {
-      //   return text.length > 2; // Only if the text is longer than 2 characters
-      // })
-      // .debounce(750 /* Pause for 750ms */ )
-      // .distinctUntilChanged(); // Only if the value has changed
-
-
-  }
-
-  search () {
-
   }
 
   handleChange (e) {
-
-    var searcher = this.keyup$.next(e);
-
-    // searcher.subscribe(
-    //   function (data) {
-    //     this.setState({
-    //       search: data
-    //     });
-    //   },
-    //   function (error) {
-    //     alert('problems');
-    //   });
-
+    this.props.changeHandler(e);
   }
 
   render () {
-    console.log(this.state.search);
     return (
       <div className="autosuggest">
         <input type="text" onChange={this.handleChange.bind(this)}/>
         {
-          this.state.search.map((d) => {
+          this.props.data.map((d) => {
             return (
               <div key={d.imdbID}>{d.title}</div>
             );
--- app/views/Autosuggest/index.jsx
+++ app/views/Autosuggest/index.jsx
@@ -1,34 +1,50 @@
 import React from 'react';
+import Rx, {Subject} from 'rxjs/Rx';
+import axios from 'axios';
 const { array } = React.PropTypes;
-import { connect } from 'react-redux';
-import { FetchShows } from './../../store/actions';
 import AutoSuggest from './../../components/AutoSuggest';
 
-const Autosuggest = React.createClass({
+class Autosuggest extends React.Component{
 
+  constructor(props) {
+    super(props);
+    this.state = {search: []};
+  }
 
   componentDidMount () {
+    this.keyup$ = new Subject();
+    this.keyup$.map((e) => {
+      return e.target.value;
+    })
+    .filter((val) => {
+      return val.length > 2;
+    })
+    .debounceTime(750)
+    .distinctUntilChanged()
+    .subscribe((val) => {
+      return axios.get(`http://localhost:3000/shows?q=${val}`).then((d) => {
+        this.setState({search: d.data});
+      });
 
-  },
+    });
+  }
+
+  componentWillUnmount () {
+    this.keyup$.unsubscribe();
+  }
+
+  handleChange (e) {
+    this.keyup$.next(e);
+  }
 
   render (){
     return (
       <div>
-        <AutoSuggest />
+        <AutoSuggest changeHandler={this.handleChange.bind(this)} data={this.state.search} />
       </div>
     );
   }
-});
-
-// Subscribe component to redux store and merge the state into
-// component's props
-const mapStateToProps = ({ showsReducer }) => {
-  console.log(showsReducer);
-  return {
- shows: showsReducer.shows
-};
 };
 
-// connect method from react-router connects the component with redux store
-export default connect(
- mapStateToProps)(Autosuggest);
+
+export default Autosuggest;
